ff0d80268c548ef2f5f7e13e75eb72902ba4b8db
Luiz Gavioli <luizbag@gmail.com>
2014-09-26 11:59:03 -0300

    Mais testes e finalizada a paralelização

--- ValidationIndex/VIIndex.cpp
+++ ValidationIndex/VIIndex.cpp
@@ -6,6 +6,11 @@
 #include <pthread.h>
 #include <stdio.h>
 
+#define qtdThreads 3
+double resposta[qtdThreads];
+Partition* partitions;
+double mutual_summation=0;
+
 void* entropy_thread(void* attr) {
     Partition* objPartition = (Partition*)attr;
     double dSummation = 0;
@@ -34,14 +39,23 @@ double intersection(Partition &objPartition1, Partition &objPartition2, Partitio
     return iCounter / static_cast<double> (objPartition1.getNumObjects());
 }
 
-void* mutual_thread(void* attr) {
-    double dSummation = 0;
-    double dStore = 0, dProbabilityPartition1 = 0, dProbabilityPartition2 = 0;
-    Partition* partitions = (Partition*)attr;
+void* partition_calculate(void* rank) {
+    long id = (long)rank;
+    long i=0;
     Partition objPartition1 = *partitions;
     Partition objPartition2 = *(partitions+1);
+    long n = objPartition1.getNumClusters()/qtdThreads;
+    long first = n*id;
+    long last = first+n;
+    double dSummation = 0;
+    double dStore = 0, dProbabilityPartition1 = 0, dProbabilityPartition2 = 0;
+    Partition::itClustersOfPartition it1;
 
-    for(Partition::itClustersOfPartition it1 = objPartition1.begin(); it1 != objPartition1.end(); it1++){
+
+    for(i=0, it1 = objPartition1.begin(); i<first && it1 != objPartition1.end(); it1++, i++) {
+    }
+
+    for(i=first; i<last && it1 != objPartition1.end(); i++, it1++) {
         for(Partition::itClustersOfPartition it2 = objPartition1.begin(); it2 != objPartition1.end(); it2++){
             dStore = intersection(objPartition1, objPartition2, it1, it2);
 
@@ -54,8 +68,44 @@ void* mutual_thread(void* attr) {
             }
         }
     }
+    resposta[id]=dSummation;
+}
 
-    pthread_exit((void*)&dSummation);
+void* mutual_thread(void* attr) {
+    mutual_summation = 0;
+    partitions = (Partition*)attr;
+    int i=0;
+
+    pthread_t* threads = (pthread_t*)malloc(qtdThreads*sizeof(pthread_t));
+
+    for(i=0;i<qtdThreads;i++) {
+        resposta[i]=0;
+        pthread_create(&threads[i], NULL, partition_calculate, (void*)i);
+    }
+
+    for(i=0;i<qtdThreads;i++) {
+        pthread_join(threads[i], NULL);
+        mutual_summation += resposta[i];
+    }
+
+
+
+    //Partition objPartition1 = *partitions;
+    //Partition objPartition2 = *(partitions+1);
+//
+    //for(Partition::itClustersOfPartition it1 = objPartition1.begin(); it1 != objPartition1.end(); it1++){
+    //    for(Partition::itClustersOfPartition it2 = objPartition1.begin(); it2 != objPartition1.end(); it2++){
+    //        dStore = intersection(objPartition1, objPartition2, it1, it2);
+//
+    //        dProbabilityPartition1 = (*it1).getNumberOfObjects() / static_cast<double> (objPartition1.getNumObjects());
+    //        dProbabilityPartition2 = (*it2).getNumberOfObjects() / static_cast<double> (objPartition2.getNumObjects());
+//
+    //        if (dStore != 0 && dStore / (dProbabilityPartition1 * dProbabilityPartition2) != 0) {
+    //            dStore *= log10(dStore / ((dProbabilityPartition1 * dProbabilityPartition2)));
+    //            dSummation += dStore;
+    //        }
+    //    }
+    //}
 }
 
 /* Implementation of both inherited methods */
@@ -69,31 +119,35 @@ double VIIndex::calculate(Partition &objAPartition1, Partition &objAPartition2)
     Partition partition[2] = {objAPartition1, objAPartition2};
     pthread_t* entropies = (pthread_t*)malloc(2*sizeof(pthread_t));
     pthread_t* mutual = (pthread_t*)malloc(sizeof(pthread_t));
-    //printf("-------comeÁo-----\n");
+    //printf("-------come√ßo-----\n");
     GET_TIME(start);
+    //Entropia
     pthread_create(&entropies[0], NULL, entropy_thread, (void*)&objAPartition1);
     pthread_create(&entropies[1], NULL, entropy_thread, (void*)&objAPartition2);
-    pthread_create(mutual, NULL, mutual_thread, (void*)&partition);
-    pthread_join(*mutual, (void**)&mut);
     pthread_join(entropies[0], (void**)&e1);
     pthread_join(entropies[1], (void**)&e2);
+    //Mutual
+    pthread_create(mutual, NULL, mutual_thread, (void*)&partition);
+    pthread_join(*mutual, NULL);
     GET_TIME(finish);
     paralel = finish-start;
     //printf("Thread foi\n");
     GET_TIME(start);
     en1 = entropy(objAPartition1);
-    //printf("entropy 1 j· foi tambÈm\n");
+    //printf("entropy 1 j√° foi tamb√©m\n");
     en2 = entropy(objAPartition2);
     muts = mutualInformation(objAPartition1, objAPartition2);
     GET_TIME(finish);
     serial = finish-start;
+    //printf("Valor Serial: %lf\nValor Paralelo: %lf\n", muts, mutual_summation);
     printf("Elapsed serial: %lf\nElapsed thread: %lf\n",serial, paralel);
     printf("----------------------\n");
-	return 0;//en1 + en2 - 2*mutualInformation(objAPartition1, objAPartition2);
+	return en1 + en2 - 2*mutualInformation(objAPartition1, objAPartition2);
+    //return *e1 + *e2 - 2*mutual_summation;
 
 }
 
 double VIIndex::calculate(Partition *pAPartition, RelationSDN *pARelation, DataSet *pADataset){
-	//TODO Colocar no relatório esse retorno com número qualquer
+	//TODO Colocar no relat‚Äîrio esse retorno com n≈ìmero qualquer
 	return -100;
 }
